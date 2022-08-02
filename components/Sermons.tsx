fc3105cd68d9f551b197003c97cd38da3c02458b
Udoka <udochukwukaonyela@gmail.com>
2022-08-02 21:45:20 +0100

    styling sermon page

--- components/Sermons.tsx
+++ components/Sermons.tsx
@@ -3,9 +3,12 @@ import  Image  from 'next/image';
 import { wave1 } from "../public";
 const Sermons:FC = () => {
   return (
-    <div>
-    <h1>Weekly Sermons</h1>
+    <section className="box-border block">
+      <span className="-z-10 w-full block"><Image src={wave1} layout={"responsive"} /></span>
+      <div className="-translate-y-[35rem]">
+        <h1 className="text-center text-bold text-white font-bold tracking-widest text-xl after:content-[''] after:absolute after:w-24 after:h-1 after:rounded-full  after:bg-white after:right-1/2 after:mt-8">Weekly Sermons</h1>
     </div>
+      </section>
   )
 }
 
