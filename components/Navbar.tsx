4ea143f2c845a22c018a1a28a5166aa4d53f7025
Udoka <udochukwukaonyela@gmail.com>
2022-08-02 19:14:57 +0100

    added navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,10 @@
 import React, { FC } from 'react'
-
+import  Image from 'next/image';
+import { logo } from '../public';
 const Navbar:FC = (props:any) => {
   return (
-    <nav >
-      
+    <nav className="">
+      <span className="w-8 h-8"><Image src={logo} /></span>
     </nav>
   )
 }
