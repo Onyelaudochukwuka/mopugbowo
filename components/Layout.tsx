9e0064d73845b2ca72cd205d98cb45b556feb953
Udoka <udochukwukaonyela@gmail.com>
2022-08-02 18:51:55 +0100

    added layout configurations

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import React, { Component, PropsWithChildren } from 'react'
 
 const Layout = ({children}:PropsWithChildren<Component>) => {
   return (
       <div className="min-h-screen">
-          
+      <Head>
+        
+      </Head>
+      {children}
     </div>
   )
 }
