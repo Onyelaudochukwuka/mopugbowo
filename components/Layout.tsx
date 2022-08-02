d4c58696ecf6f645a6f9c69f1e6927a46af58e80
Udoka <udochukwukaonyela@gmail.com>
2022-08-02 18:58:17 +0100

    created layouts

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,7 +5,15 @@ const Layout = ({children}:PropsWithChildren<Component>) => {
   return (
       <div className="min-h-screen">
       <Head>
-        
+          <meta name="author" content="Onyela Udochukwuka"/>
+            <meta name="description" content="Ministry Of Perfection - Ugbowo; Stay up-to-date with Our programmes and Event"/>
+              <meta name="keywords" content="Ministry Of Perfection"/>
+                <meta http-equiv='X-UA-Compatible' content='IE=edge'/>
+                  <meta property="og:image" content="./logo - Copy.png"/>
+                    <meta property="og:description" content="Ministry Of Perfection - Ugbowo; Stay up-to-date with Our programmes and Event"/>
+                      <meta property="og:title" content="Ministry Of Perfection Church - Ugbowo"/>
+                        <title>Ministry Of Perfection Church - Ugbowo</title>
+                        <meta name='viewport' content='width=device-width, initial-scale=1'/>
       </Head>
       {children}
     </div>
