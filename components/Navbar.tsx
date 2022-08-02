7172164227e5be885dfeb2a6497faba6002b84c5
Udoka <udochukwukaonyela@gmail.com>
2022-08-02 19:17:58 +0100

    styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import  Image from 'next/image';
 import { logo } from '../public';
 const Navbar:FC = (props:any) => {
   return (
-    <nav className="">
+    <nav className="bg-curves w-full flex flex-1">
       <span className="w-8 h-8"><Image src={logo} /></span>
     </nav>
   )
