96508562b2aec3e47e8bb10f7d4af63025431c25
Udoka <udochukwukaonyela@gmail.com>
2022-08-02 18:13:28 +0100

    added layout props

---
+++ components/Layout.tsx
@@ -0,0 +1,11 @@
+import React, { Component, PropsWithChildren } from 'react'
+
+const Layout = ({children}:PropsWithChildren<Component>) => {
+  return (
+      <div className="min-h-screen">
+          
+    </div>
+  )
+}
+
+export default Layout;
