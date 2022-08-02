a3a4f8a4d81d95e234f347fd2a817097f189380a
Udoka <udochukwukaonyela@gmail.com>
2022-08-02 20:21:56 +0100

    added background

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,8 +2,8 @@ import React, { FC } from 'react'
 
 const Hero:FC = () => {
   return (
-    <div className="h-screen bg-background">
-
+    <div className="h-screen bg-background bg-center bg-cover bg-no-repeat">
+fff
     </div>
   )
 }
