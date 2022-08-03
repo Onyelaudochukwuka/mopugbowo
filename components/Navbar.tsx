6190686093e7c7b342dd37388dc46f79e641695c
Udoka <udochukwukaonyela@gmail.com>
2022-08-03 08:15:40 +0100

    added positioning

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import { logo } from '../public';
 import Link from "next/link";
 const Navbar:FC = (props:any) => {
   return (
-    <nav className="bg-curves w-full flex flex-1 p-4 justify-between items-center px-[8.33%] fixed">
+    <nav className="bg-curves w-full flex flex-1 p-4 justify-between items-center px-[8.33%] fixed z-50">
       <span className="w-12 "><Image src={logo} /></span>
       <div className="flex justify-between basis-6/12 mr-[8.33%] text-white font-bold items-center">
         <span>Home</span>
