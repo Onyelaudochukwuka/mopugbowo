88a98b9bdf4ddb896e7ed09fa078ccafee1ea41a
Udoka <udochukwukaonyela@gmail.com>
2022-08-04 10:52:19 +0100

    exported imagesz

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 import logo from "./logo.png";
 import wave1 from "./wave1.svg";
-import slide1 from './slide1.png'
+import slide1 from './slide1.jpeg'
 import slide2 from './slide2.png'
 import slide3 from './slide3.png'
 
