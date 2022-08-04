907a55a0a9865760f8583f598ad14717d868a6c7
Udoka <udochukwukaonyela@gmail.com>
2022-08-04 16:17:28 +0100

    styled aboutUs page

--- components/About.tsx
+++ components/About.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image"
 import React, { FC } from 'react'
 import { pastor, about1, about2, about3 } from "../public";
-const about = [about1, about2, about3];
+const about = [about1, about2, about2];
 const About:FC = () => {
   return (
     <section className="mt-24">
-    <div className="flex justify-around">
+    <div className="flex justify-around items-center">
       <span className="h-1/3 block basis-1/3"><Image src={pastor} layout={"responsive"} /></span> 
       <div className="w-full basis-1/3 text-left text-white flex flex-col gap-3">
         <h1 className="text-white font-bold tracking-widest text-2xl before:content-[''] before:absolute before:w-24 before:h-1 before:rounded-full  before:bg-white  before:mt-8">About US</h1>
@@ -15,8 +15,8 @@ const About:FC = () => {
         </div>
       </div>
       </div>
-      <div>
-        {about.map((img) => <span><Image src={img} layout={"responsive"} /></span>)}
+      <div className="flex justify-evenly w-3/4 ml-auto relative right-0 -translate-y-10">
+        {about.map((img) => <span className="w-48 h-48 block cursor-pointer"><Image src={img} layout={"responsive"} /></span>)}
       </div>
     </section>
   )
