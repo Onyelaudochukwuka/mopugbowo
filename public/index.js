a56fabd3d4d5e4e0d22b00b3d3c57aea512f157c
Udoka <udochukwukaonyela@gmail.com>
2022-08-04 14:35:28 +0100

    exported images

--- public/index.js
+++ public/index.js
@@ -5,5 +5,9 @@ import slide2 from './slide2.jpeg';
 import slide3 from './slide3.jpeg';
 import leftArrow from './leftArrow.svg';
 import rightArrow from './rightArrow.svg';
+import about1 from './about1.png';
+import about2 from './about2.png';
+import about3 from './about3.png';
 
-export { logo, wave1, slide1, slide2, slide3, rightArrow, leftArrow };
+
+export { logo, wave1, slide1, slide2, slide3, rightArrow, leftArrow, about1, about2, about3 };
