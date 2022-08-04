7043545ae1857a62bc9590c202c850c7401ef607
Udoka <udochukwukaonyela@gmail.com>
2022-08-04 10:43:51 +0100

    exported slides

--- public/index.js
+++ public/index.js
@@ -1,4 +1,7 @@
 import logo from "./logo.png";
 import wave1 from "./wave1.svg";
+import slide1 from './slide1.png'
+import slide2 from './slide2.png'
+import slide3 from './slide3.png'
 
-export { logo, wave1 };
+export { logo, wave1, slide1,slide2,slide3 };
