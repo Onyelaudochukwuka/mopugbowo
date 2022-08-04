fd547843944c251a84783160c81309695d945302
Udoka <udochukwukaonyela@gmail.com>
2022-08-04 10:18:09 +0100

    created a slide component

--- components/Sermons.tsx
+++ components/Sermons.tsx
@@ -1,62 +1,14 @@
 import React, { FC } from 'react'
 import  Image  from 'next/image';
 import { wave1 } from "../public";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectCoverflow, Pagination } from "swiper";
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
+
 const Sermons:FC = () => {
   return (
     <section className="box-border block">
       <span className="-z-10 w-full block"><Image src={wave1} layout={"responsive"} /></span>
       <div className="-translate-y-[35rem]">
         <h1 className="text-center text-bold text-white font-bold tracking-widest text-xl after:content-[''] after:absolute after:w-24 after:h-1 after:rounded-full  after:bg-white after:right-1/2 after:mt-8 ">Weekly Sermons</h1>
-        <Swiper
-          effect={"coverflow"}
-          grabCursor={true}
-          centeredSlides={true}
-          slidesPerView={"auto"}
-          coverflowEffect={{
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true,
-          }}
-          pagination={true}
-          modules={[EffectCoverflow, Pagination]}
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-          </SwiperSlide>
-        </Swiper>
+      
     </div>
       </section>
   )
---
+++ components/Slides.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+const Slides:FC = () => {
+  return (
+    <div>Slides</div>
+  )
+}
+
+export default Slides
