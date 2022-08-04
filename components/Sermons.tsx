5d71c34ad9fb4844a97a0ccf55ea4035d76c3d25
Udoka <udochukwukaonyela@gmail.com>
2022-08-04 14:25:41 +0100

    created sermons page

--- components/Sermons.tsx
+++ components/Sermons.tsx
@@ -5,12 +5,12 @@ import Slides from "./Slides";
 
 const Sermons:FC = () => {
   return (
-    <section className="box-border block">
-      <div className="z-20 absolute w-full p-8">
+    <section className="box-border block bg-dark h-max max-h-screen min-h-[90vh]">
+      <span className="w-full block  absolute"><Image src={wave1} layout={"responsive"} /></span>
+      <div className="z-20 relative w-full p-8">
         <h1 className="text-center text-bold text-white font-bold tracking-widest text-xl after:content-[''] after:absolute after:w-24 after:h-1 after:rounded-full  after:bg-white after:right-1/2 after:mt-8 ">Weekly Sermons</h1>
       <Slides/>
     </div>
-      <span className="-z-50 w-full block  absolute"><Image src={wave1} layout={"responsive"} /></span>
     </section>
   )
 }
