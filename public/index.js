e1e188b4b110467d511e3cad9a7e8e7076fd145e
Udoka <udochukwukaonyela@gmail.com>
2022-08-04 10:55:50 +0100

    exports

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 import logo from "./logo.png";
 import wave1 from "./wave1.svg";
-import slide1 from './slide1.jpeg'
-import slide2 from './slide2.jpeg'
-import slide3 from './slide3.png'
+import slide1 from './slide1.jpeg';
+import slide2 from './slide2.jpeg';
+import slide3 from './slide3.jpeg';
 
 export { logo, wave1, slide1,slide2,slide3 };
