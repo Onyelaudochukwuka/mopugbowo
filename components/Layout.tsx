e83d80b417f9e9d39193c96419d26ac2bf946e7d
Udoka <udochukwukaonyela@gmail.com>
2022-08-04 15:55:37 +0100

    increase performace and loadspeed with font family

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,13 +9,16 @@ const Layout = ({children}:PropsWithChildren<any>) => {
       <Head>
           <meta name="author" content="Onyela Udochukwuka"/>
             <meta name="description" content="Ministry Of Perfection - Ugbowo; Stay up-to-date with Our programmes and Event"/>
-              <meta name="keywords" content="Ministry Of Perfection"/>
+              <meta name="keywords" content="Ministry Of Perfection,ministry,perfection,church,ugbowo,Paul Abass"/>
                 <meta http-equiv='X-UA-Compatible' content='IE=edge'/>
                   <meta property="og:image" content="./logo - Copy.png"/>
                     <meta property="og:description" content="Ministry Of Perfection - Ugbowo; Stay up-to-date with Our programmes and Event"/>
                       <meta property="og:title" content="Ministry Of Perfection Church - Ugbowo"/>
                         <title>Ministry Of Perfection Church - Ugbowo</title>
-                        <meta name='viewport' content='width=device-width, initial-scale=1'/>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+            <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
       </Head>
       <Navbar />
       {children}
