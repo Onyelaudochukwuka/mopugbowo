e9717e7cf27651c5e495386cd8ebf966405e799a
Udoka <udochukwukaonyela@gmail.com>
2022-08-04 10:39:35 +0100

    created sermon component

--- components/Sermons.tsx
+++ components/Sermons.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import  Image  from 'next/image';
 import { wave1 } from "../public";
+import Slides from "./Slides";
 
 const Sermons:FC = () => {
   return (
@@ -8,7 +9,7 @@ const Sermons:FC = () => {
       <span className="-z-10 w-full block"><Image src={wave1} layout={"responsive"} /></span>
       <div className="-translate-y-[35rem]">
         <h1 className="text-center text-bold text-white font-bold tracking-widest text-xl after:content-[''] after:absolute after:w-24 after:h-1 after:rounded-full  after:bg-white after:right-1/2 after:mt-8 ">Weekly Sermons</h1>
-      
+      <Slides/>
     </div>
       </section>
   )
