1aaea5d1066eae306a9e60612e711a52d1df91e8
Udoka <udochukwukaonyela@gmail.com>
2022-08-04 10:52:12 +0100

    downloaded slides

--- components/Slides.tsx
+++ components/Slides.tsx
@@ -1,9 +1,13 @@
-import React, { FC } from 'react'
-
-const Slides:FC = () => {
+import Image from "next/image";
+import React, { FC, useId } from 'react'
+import { slide1, slide2, slide3 } from "../public";
+const images = [slide2, slide1, slide3];
+const Slides: FC = () => {
+  const Id = useId()
   return (
-      <div>
-          
+      <div className="flex">
+      {images.map((img,i) =>  <Image key={Id + i} src={img}/>
+          )}
     </div>
   )
 }
