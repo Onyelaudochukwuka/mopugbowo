f1c70c8efa9a3bd73d384368f6d592cc24b35afa
Udoka <udochukwukaonyela@gmail.com>
2022-08-04 15:35:54 +0100

    styling

--- components/About.tsx
+++ components/About.tsx
@@ -1,9 +1,13 @@
+import Image from "next/image"
 import React, { FC } from 'react'
-
+import { pastor } from "../public"
 const About:FC = () => {
   return (
-    <section>
-      
+    <section className="flex">
+      <span className="w-1/3 block"><Image src={pastor} layout={"responsive"} /></span> 
+      <div>
+        
+      </div>
     </section>
   )
 }
