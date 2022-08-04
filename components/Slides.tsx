6f197599cd3ba6a9d33a864295a2d9d1d2d5c2f2
Udoka <udochukwukaonyela@gmail.com>
2022-08-04 11:14:44 +0100

    created slides

--- components/Slides.tsx
+++ components/Slides.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import React, { FC, useId } from 'react'
 import { slide1, slide2, slide3 } from "../public";
-const images = [slide2, slide1, slide3];
+const images = [slide3, slide1, slide3];
 const Slides: FC = () => {
   const Id = useId()
   return (
-      <div className="flex">
-      {images.map((img,i) =>  <Image key={Id + i} src={img}/>
+      <div className="flex items-baseline">
+      {images.map((img, i) => <span key={Id + i} className={`${i  % 2 !== 1 ? `scale-75 ${(i+1) % 3 !== 0 ? `translate-x-36` : `-translate-x-36` }` : `scale-150 z-20`}`}><Image src={img} /></span>
           )}
     </div>
   )
