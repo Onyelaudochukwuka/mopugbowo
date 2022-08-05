b79d51a71ecdb2516baf08c66664c0613b83d226
Udoka <udochukwukaonyela@gmail.com>
2022-08-05 10:30:50 +0100

    fixes

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,7 +2,10 @@ import React, { FC } from 'react'
 
 const Contact:FC = () => {
   return (
-    <div>Contact</div>
+    <section>
+      <h1 className="text-center text-bold text-white font-bold tracking-widest text-2xl before:content-[''] before:absolute before:w-24 before:h-1 before:rounded-full  before:bg-white before:mt-8 ">Contact Us</h1>
+
+    </section>
   )
 }
 
