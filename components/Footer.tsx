248d8fc9ed424f2954e55aa5ce575b20e623983e
Udoka <udochukwukaonyela@gmail.com>
2022-08-05 15:37:25 +0100

    downloaded footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,14 @@
-import React, { FC } from 'react'
-
+import React, { FC } from 'react';
+import Image from "next/image";
+import { footerimg } from "../public";
 const Footer:FC = (props:any) => {
   return (
-    <div>Footer</div>
+    <section className="box-border block bg-dark h-max max-h-screen min-h-[90vh]">
+      <span className="w-full block  absolute"><Image src={footerimg} layout={"responsive"} /></span>
+      <div className="relative z-50 mt-auto bottom-0 text-white translate-y-[90vh] ">
+          <p>Hiii</p>
+      </div>
+      </section>
   )
 }
 
