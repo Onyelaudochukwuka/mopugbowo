d965252414dffe1442db41e13898f4392420e3a6
Udoka <udochukwukaonyela@gmail.com>
2022-08-06 10:15:56 +0100

    styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,12 @@
 import React, { FC, useId } from 'react';
 import Image, { StaticImageData } from "next/image";
-import { footerimg, facebook, twitter, instagram } from "../public";
+import { footerimg, facebook, twitter, instagram, logo } from "../public";
 const socials:StaticImageData[] = [facebook, twitter, instagram];
 const Footer: FC = () => {
   const Id = useId();
   return (
-    <section className="box-border block bg-dark h-max max-h-screen min-h-[90vh] relative">
-      <div className="absolute z-50 text-white bottom-0 w-full">
+    <section className="box-border block bg-dark h-max  relative">
+      <div className="absolute z-50 text-white bottom-24 w-full flex flex-col gap-12 items-center self-end mt-auto">
         <div className="flex justify-between w-3/4 mx-auto font-semibold">
           <span>Home</span>
           <span>What We Believe</span>
@@ -14,7 +14,12 @@ const Footer: FC = () => {
           <span>Locations</span>
           <span>Contact</span>
         </div>
-        <div>{socials.map((icon:StaticImageData,i:number)=><Image key={Id+i} src={icon} />)}</div>
+        <div className="flex items-center justify-between w-3/12">{socials.map((icon: StaticImageData, i: number) => <span key={Id + i} className="w-10 h-10 block"><Image src={icon} layout="responsive" /></span>)}</div>
+        <p>&copy;2020 Ministry Of Perfection CHurch  All Right Reserved</p>
+        <span className="block w-12 h-12">
+          <Image src={logo} layout="responsive"/>
+        </span>
+
       </div>
       <span className="w-full block"><Image src={footerimg} layout={"responsive"} /></span>
       </section>
