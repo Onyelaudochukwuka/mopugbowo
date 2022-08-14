f071718593664aadd6a85d5059bbef3b72419770
Udoka <udochukwukaonyela@gmail.com>
2022-08-14 14:23:27 +0100

    created display card componet

---
+++ components/Card.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+      <div className="fixed inset-0 bg-gray-200/50 z-[99] flex justify-center items-center">
+          <div className="bg-white w-3/4 h-3/4 flex justify-center flex-col items-center">
+              <h1>Experience Koinonia</h1>
+              <p>We enjoy fellowship together every week at any of our several locations all over the country. join us this weekend at a location close to you.</p>
+              <a>Plan A Visit</a>
+          </div>
+    </div>
+  )
+}
+
+export default Card
