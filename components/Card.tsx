4d021058c73ec8607852d0ab573bc49af43aca32
Udoka <udochukwukaonyela@gmail.com>
2022-08-14 14:32:49 +0100

    styled card

--- components/Card.tsx
+++ components/Card.tsx
@@ -4,8 +4,8 @@ const Card = () => {
   return (
       <div className="fixed inset-0 bg-gray-200/50 z-[99] flex justify-center items-center">
       <div className="bg-white w-1/2 h-1/2 m-auto flex flex-col items-center shadow-inner rounded-lg justify-evenly px-12 text-center">
-              <h1>Experience Koinonia</h1>
-              <p>We enjoy fellowship together every week at any of our several locations all over the country. join us this weekend at a location close to you.</p>
+              <h1 className="text-2xl font-bold">Experience Koinonia</h1>
+              <p className="font-semibold text-lg">We enjoy fellowship together every week at any of our several locations all over the country. join us this weekend at a location close to you.</p>
               <a className="p-8 py-4 bg-curves rounded-full text-white font-bold">Plan A Visit</a>
           </div>
     </div>
