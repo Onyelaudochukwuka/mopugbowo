a3065450cbc7205db00c40ba312b5402b07f0403
Udoka <udochukwukaonyela@gmail.com>
2022-08-14 14:46:28 +0100

    added cancel button

--- components/Card.tsx
+++ components/Card.tsx
@@ -4,11 +4,11 @@ import { Cancel } from "../public"
 const Card = () => {
   return (
     <div className="fixed inset-0 bg-gray-200/50 z-[99] flex justify-center items-center">
-      <Cancel />
-      <div className="bg-white w-1/2 h-1/2 m-auto flex flex-col items-center shadow-inner rounded-lg justify-evenly px-12 text-center">
+      <div className="bg-white w-1/2 h-1/2 m-auto flex flex-col items-center shadow-inner rounded-lg justify-center gap-4 px-12 text-center relative">
+      <Cancel className="w-8 h-8 absolute top-4 right-5 cursor-pointer"/>
               <h1 className="text-2xl font-bold">Experience Koinonia</h1>
               <p className="font-semibold text-lg">We enjoy fellowship together every week at any of our several locations all over the country. join us this weekend at a location close to you.</p>
-              <a className="p-8 py-4 bg-curves rounded-full text-white font-bold">Plan A Visit</a>
+              <a className="p-8 py-4 bg-curves rounded-full text-white font-bold cursor-pointer">Plan A Visit</a>
           </div>
     </div>
   )
