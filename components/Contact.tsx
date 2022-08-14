d8f39f7db4d498afeb44c53fc73220b7940f13d5
Udoka <udochukwukaonyela@gmail.com>
2022-08-14 13:54:21 +0100

    fixes

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -32,7 +32,7 @@ const Contact: FC = () => {
       <h1 className="text-center relative text-bold text-white font-bold tracking-widest text-2xl before:absolute before:w-24 before:h-1 before:rounded-full  before:bg-white before:mt-8 before:origin-left before:transition-[transform] before:duration-300 hover:before:scale-x-125  cursor-pointer">Contact Us</h1>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.107868914145!2d5.604486662757621!3d6.380075390984419!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcb4f67723b60b2ea!2sMinistry%20Of%20Perfection%20-%20Ugbowo!5e0!3m2!1sen!2sng!4v1653662210346!5m2!1sen!2sng"
-        className="m-auto w-4/5 h-[500px]" loading="lazy" />
+        className="mx-auto w-4/5 h-[500px]" loading="lazy" />
       <div className="flex justify-around gap-12 w-11/12 mx-auto lg:flex-row flex-col">
         {contactDetails.map(({ icon, heading, content }, i) =>
           <span key={Id + i} className="flex flex-col items-center justify-center gap-2 lg:p-3 p-12 bg-curves basis-1/5 w-full flex-1 text-center text-white rounded-lg shadow-[0px_4px_8px_rgba(0,0,0,0.25)]">
