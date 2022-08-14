590ab765a9ba8d5718c497ba7e738acda9f6b0e4
Udoka <udochukwukaonyela@gmail.com>
2022-08-14 20:39:34 +0100

    created added custom hook

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { FC, PropsWithChildren } from 'react'
 import { Cancel } from "../public"
-
-const Card = () => {
+import { Data } from './About';
+const Card = ({ changeState, data }:  { changeState:() => void, data: Data}) => {
   return (
     <div className="fixed inset-0 bg-gray-200/50 z-[99] flex justify-center items-center">
       <div className="bg-white w-1/2 h-1/2 m-auto flex flex-col items-center shadow-inner rounded-lg justify-center gap-4 px-12 text-center relative">
