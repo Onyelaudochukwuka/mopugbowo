19d8e6294d6fb5e257ac54931f92aa9a4ff90b89
Udoka <udochukwukaonyela@gmail.com>
2022-08-14 14:23:38 +0100

    exported card xomponbent

--- components/index.js
+++ components/index.js
@@ -6,3 +6,4 @@ export { default as Hero } from './Hero';
 export { default as Layout } from './Layout';
 export { default as Navbar } from './Navbar';
 export { default as Sermons } from './Sermons';
+export { default as Card } from './Card';
