cd1ae8d02a1592bfc68c7c58bd984484c3adf0e1
Udoka <udochukwukaonyela@gmail.com>
2022-08-15 08:23:43 +0100

    created arrow componnt

--- public/Arrow.tsx
+++ public/Arrow.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Arrow = (props:any) => {
   return (
       <svg {...props} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M15 18L21 12L15 6" stroke="#0C0114" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-          <path d="M3 12H20" stroke="#0C0114" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-          <path d="M21 12H20" stroke="#0C0114" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M15 18L21 12L15 6" stroke="#0C0114" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+          <path d="M3 12H20" stroke="#0C0114" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+          <path d="M21 12H20" stroke="#0C0114" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
       </svg>
 
   )
