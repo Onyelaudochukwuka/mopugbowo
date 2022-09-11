7ffc5e2f84cf8b98223c1920b9c18cc7a654987d
Udoka <udochukwukaonyela@gmail.com>
2022-09-11 21:10:01 +0100

    created page template component

---
+++ template/page.tsx
@@ -0,0 +1,13 @@
+import  React, { FC } from 'react';
+
+export interface IblockNameProps {
+}
+
+const blockName: FC<IblockNameProps> = (props) => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+export default blockName;
