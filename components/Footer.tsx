f604389db7ba60cb69ef10f489c01a61e998b387
Udoka <udochukwukaonyela@gmail.com>
2022-09-11 16:46:51 +0100

    mobile styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,23 +5,23 @@ const socials:StaticImageData[] = [facebook, twitter, instagram];
 const Footer: FC = () => {
   const Id = useId();
   return (
-    <section className="box-border block bg-dark h-max  relative">
-      <div className="absolute z-10 text-white lg:bottom-24 bottom-2 w-full flex flex-col gap-12 items-center self-end mt-auto">
-        <div className="flex justify-between w-3/4 mx-auto font-semibold">
+    <section className="box-border block bg-curves h-max py-8 mt-8 relative">
+      <div className="lg:absolute relative z-10 text-white lg:bottom-24 w-full flex flex-col gap-12 items-center self-end mt-auto">
+        <div className="flex justify-between w-3/4 mx-auto flex-col lg:flex-row gap-6 font-semibold">
           <span>Home</span>
           <span>What We Believe</span>
           <span>Sermons</span>
           <span>Locations</span>
           <span>Contact</span>
         </div>
-        <div className="flex items-center justify-between w-3/12">{socials.map((icon: StaticImageData, i: number) => <span key={Id + i} className="w-10 h-10 block"><Image src={icon} layout="responsive" /></span>)}</div>
+        <div className="flex items-center justify-between lg:w-3/12 w-3/4">{socials.map((icon: StaticImageData, i: number) => <span key={Id + i} className="w-10 h-10 block"><Image src={icon} layout="responsive" /></span>)}</div>
         <p className="font-bold">&copy;2020 Ministry Of Perfection CHurch  All Right Reserved</p>
         <span className="block w-12 h-12">
           <Image src={logo} layout="responsive"/>
         </span>
 
       </div>
-      <span className="w-full block"><Image src={footerimg} layout={"responsive"} /></span>
+      <span className="w-full lg:block hidden"><Image src={footerimg} layout={"responsive"} /></span>
       </section>
   )
 }
