a5df9496aa8fa573665bf90042bad780c81d0bb8
Udoka <udochukwukaonyela@gmail.com>
2022-09-11 16:19:37 +0100

    added animations

--- components/About.tsx
+++ components/About.tsx
@@ -42,13 +42,15 @@ const About: FC = () => {
   const [show, setShow] = useState<boolean>(false);
   const [data, setData] = useState<Data>({ heading: "", details: "", link: "" });
   const imageContainerEl = useRef<HTMLHeadingElement>(null);
-  const isInView = useInView(imageContainerEl, { once: false,
+  const isInView = useInView(imageContainerEl, {
+    once: false,
     margin: "75% 100px 75% 0px"
-  })
+  });
+  const textIsInView = useInView(imageContainerEl, { once: false });
   const useSetShow = (): void => {
     return setShow((state) => !state);
   };
-  const { span: Span } = motion;
+  const { span: Span, div: Div } = motion;
   console.log(isInView)
   return (
     <section className="mt-24">
@@ -65,13 +67,21 @@ const About: FC = () => {
           }}
           transition={{delay: 0, type: "spring", stiffness: 140, bounce: 200}}
           className="lg:h-1/3 block lg:basis-1/3 lg:w-full w-3/4 h-auto"><Image src={pastor} layout={"responsive"} /></Span> 
-      <div className="w-full basis-1/3 text-left text-white flex flex-col gap-3 p-6 lg:p-0">
+        <Div
+          initial={{
+            opacity: 0
+          }}
+          animate={{
+            opacity: textIsInView ? 1 : 0
+          }}
+          transition={{ delay: 0, type: "spring"}}
+          className="w-full basis-1/3 text-left text-white flex flex-col gap-3 p-6 lg:p-0">
           <h1 className="text-white font-bold tracking-widest text-2xl before:content-[''] before:absolute before:w-16 before:h-1 before:rounded-full  before:bg-white  before:mt-8  before:origin-left before:transition-[transform] before:duration-300 hover:before:scale-x-125 w-fit cursor-pointer" ref={imageContainerEl}>About US</h1>
         <div>
           <h2 className="text-white font-semibold tracking-widest text-xl">Why We Exist</h2>
           <p>Our mission is to bring total salvation and transformation to all men by teaching and preaching the undiluted word of God across the world.</p>
         </div>
-      </div>
+      </Div>
       </div>
       <div className="flex lg:justify-evenly lg:w-3/4 ml-auto relative right-0 lg:-translate-y-10 w-full justify-between p-6">
         {about.map(({ icon, data }:About, i) => <span key={Id + i} className="w-full h-full block cursor-pointer" onClick={() => { 
