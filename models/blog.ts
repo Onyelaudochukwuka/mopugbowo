4e10e49d2e10f0b979073d54731b5c6bcf15e8ec
Udoka <udochukwukaonyela@gmail.com>
2022-09-11 22:54:24 +0100

    added types for blog post

--- models/blog.ts
+++ models/blog.ts
@@ -2,13 +2,20 @@ import {Schema, model, models} from "mongoose";
 interface BlogPost{
         date: number;
         title: string;
+        post: {
+                type: string,
+                children: { text: string }[]
+
+        }[]
 }
 const blogSchema = new Schema<BlogPost>({
         date: {
                 type: Number,
                 default: Date.now()
         },
-        title: String
+        title: String,
+        post: [{ type: String, children: [{ text: String }] }]
+        
 });
 
 module.exports = models.Url || model('Url', blogSchema);
