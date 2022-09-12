fc76afc7508aceaef1c9c983c1c020749668bf31
Udoka <udochukwukaonyela@gmail.com>
2022-09-12 18:52:19 +0100

    added success icon

--- components/PopUp.tsx
+++ components/PopUp.tsx
@@ -1,13 +1,17 @@
 import  React, { FC } from 'react';
+import Success from "./icon/Success";
 
 export interface IPopUpProps {
 }
 
 const PopUp: FC<IPopUpProps> = (props) => {
   return (
-    <div>
-      
-    </div>
+    <section className="flex fixed inset-0 w-full h-full z-50 items-center justify-center">
+      <div className="bg-white w-3/4 h-3/4 flex items-center justify-center flex-col">
+        <p className="text-xl font-bold"><Success /> </p>
+        <p>Post Created</p>
+      </div>
+    </section>
   );
 }
 export default PopUp;
