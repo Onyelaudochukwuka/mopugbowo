fe0f4b8c17533a8ff2757cf0c044a8760270498a
Udoka <udochukwukaonyela@gmail.com>
2022-09-12 17:30:55 +0100

    fixed transition change on screen size change

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar: FC = () => {
   return (
     <nav className={`${yDistance > 100 ? `bg-curves` : `bg-transparent` } transition-colors duration-500 ease-in w-full flex flex-1 p-4 justify-between items-center px-[8.33%] ${route !== `/` ? `relative` :`fixed`} z-40`}>
       <span className="w-12 "><Image src={logo} /></span>
-      <div className={`flex justify-between lg:flex-row lg:basis-6/12 lg:mr-[8.33%] text-white font-bold items-center ${dropDown ? `scale-100 bg-curves items-center py-12` : `scale-0`} lg:bg-transparent lg:py-0 lg:scale-100 origin-top-left  transition-transform duration-700 ease-in z-50  h-[90vh] w-full lg:w-auto lg:h-auto inset-0 lg:relative absolute top-24 lg:top-auto flex-col`}>
+      <div className={`flex justify-between lg:flex-row lg:basis-6/12 lg:mr-[8.33%] text-white font-bold items-center ${dropDown ? `scale-100 bg-curves items-center py-12` : `scale-0`} lg:bg-transparent lg:py-0 lg:scale-100 origin-top-left  transition-transform duration-700 lg:transition-none lg:duration-0 ease-in z-50  h-[90vh] w-full lg:w-auto lg:h-auto inset-0 lg:relative absolute top-24 lg:top-auto flex-col`}>
         {
           content.map((value:string,index:number) => <span key={Id + index} className="relative before:absolute before:-bottom-2 before:w-full before:left-0 before:h-1 before:bg-white before:scale-x-0 before:origin-right before:transition-transform before:duration-300 hover:before:scale-100 hover:before:origin-left before:rounded-full cursor-pointer">{value}</span>
           )
