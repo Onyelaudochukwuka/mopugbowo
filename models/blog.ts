76d34982cf7141b55fdbb9dd5f64a1cd48a566f4
Udoka <udochukwukaonyela@gmail.com>
2022-09-12 14:18:28 +0100

    fixed typing

--- models/blog.ts
+++ models/blog.ts
@@ -3,16 +3,53 @@ import type { Descendant } from 'slate';
 export interface BlogPost{
         date: number;
         title: string;
-        post: Descendant[];
+        post: PostSchema[] | Descendant[];
 }
+
+interface ChildSchema {
+        text: string;
+        bold: boolean;
+        align: string;
+        code: boolean;
+        italic: boolean;
+        underline: boolean;
+}
+const childSchema = new Schema<ChildSchema>({
+        text: String,
+        bold: {
+                type: Boolean,
+                default: false
+        },
+        align: String,
+        code: {
+                type: Boolean,
+                default: false
+        },
+        italic: {
+                type: Boolean,
+                default: false
+        },
+        underline: {
+                type: Boolean,
+                default: false
+        }
+});
+interface PostSchema {
+        type: string;
+        children: ChildSchema[];
+}
+const postSchema = new Schema<PostSchema>({
+        "type": String,
+        children: [childSchema] 
+        });
 const blogSchema = new Schema<BlogPost>({
         date: {
                 type: Number,
                 default: Date.now()
         },
         title: String,
-        post: [{ type: String, children: [{ text: String }] }]
+        post: [postSchema]
         
 });
 
-module.exports = models.Url || model('Url', blogSchema);
+module.exports = models.blog || model('blog', blogSchema);
