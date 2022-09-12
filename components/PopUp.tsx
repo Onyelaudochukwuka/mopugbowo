3b95318f02a2aa61775ea34cfa911ba24248c23c
Udoka <udochukwukaonyela@gmail.com>
2022-09-12 19:11:17 +0100

    added close icon component

--- components/PopUp.tsx
+++ components/PopUp.tsx
@@ -8,6 +8,7 @@ const PopUp: FC<IPopUpProps> = (props) => {
   return (
     <section className="flex fixed inset-0 w-full h-full z-50 items-center justify-center">
       <div className="bg-white w-3/4 h-3/4 flex items-center justify-center flex-col">
+        <Close
         <p className="text-xl font-bold"><Success /> </p>
         <p>Post Created</p>
       </div>
