810b6b380b9920567f14411e9e7ed0ced849b28a
Udoka <udochukwukaonyela@gmail.com>
2022-09-12 17:31:23 +0100

    created pop up

---
+++ components/PopUp.tsx
@@ -0,0 +1,13 @@
+import  React, { FC } from 'react';
+
+export interface IPopUpProps {
+}
+
+const PopUp: FC<IPopUpProps> = (props) => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+export default PopUp;
