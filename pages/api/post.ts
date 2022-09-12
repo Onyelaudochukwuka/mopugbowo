16c1d08a8298381792d37bd5dbaadfc041125ebc
Udoka <udochukwukaonyela@gmail.com>
2022-09-12 14:19:09 +0100

    added logic for adding data

--- pages/api/post.ts
+++ pages/api/post.ts
@@ -4,21 +4,20 @@ const Blog = require('../../models/blog');
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { method } = req;
     const { post, heading, date } = req.body;
+    console.log(req.body);
     await dbConnect()
-
     switch (method) {
         case 'POST':
-            if (!!post && !!heading && !!date) { 
+
                 try {
-                    const url = await Blog.create({ post, heading, date });
-                    res.status(201).json({ success: true, data: url })
+                    const blog = await Blog.create({ post, heading, date });
+                    res.status(201).json({ success: true, data: blog })
                 }
                 catch (error: any) {
+                    console.log(error);
                     res.status(400).json({ success: false, message: error.message });
                 }
-            } else {
-                res.status(400).json({ success: false, message: "Please enter all fields" });
-            }
+            break;
         default:
             res.status(400).json({ success: false })
             break
