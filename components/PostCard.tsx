a69e030edc6700566d94464a761ff3c02c0e797a
Udoka <udochukwukaonyela@gmail.com>
2022-09-13 23:31:15 +0100

    styling

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -8,11 +8,13 @@ export interface IPostCardProps {
 
 const PostCard: FC<BlogPost> = ({ date, excerpt, image_url, post, title }) => {
   return (
-    <section>
+      <section className="flex flex-col">
+        <Image src={image_url} alt={title} width={300} height={300} />
+          <div className="flex flex-col text-white">
           <p>{moment(date).fromNow()}</p>
           <p>{title}</p>
           <p>{excerpt}</p>
-          <Image src={image_url} alt={title} width={300} height={300} />
+          </div>
     </section>
   );
 }
