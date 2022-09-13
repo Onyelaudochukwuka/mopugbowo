1d2085ef2c4b08608224622f9aae58d101cc976b
Udoka <udochukwukaonyela@gmail.com>
2022-09-13 16:24:30 +0100

    created loading component

---
+++ components/Loading.tsx
@@ -0,0 +1,13 @@
+import  React, { FC } from 'react';
+
+export interface ILoadingProps {
+}
+
+const Loading: FC<ILoadingProps> = (props) => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+export default Loading;
