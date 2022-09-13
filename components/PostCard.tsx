be842394d8e7ced93643fcc77793c3987bf05494
Udoka <udochukwukaonyela@gmail.com>
2022-09-13 23:16:23 +0100

    created post card component

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,3 +1,5 @@
+import moment from "moment";
+import Image from "next/image";
 import  React, { FC } from 'react';
 import { BlogPost } from "../models/blog";
 
@@ -7,7 +9,10 @@ export interface IPostCardProps {
 const PostCard: FC<BlogPost> = ({ date, excerpt, image_url, post, title }) => {
   return (
     <section>
-      
+          <p>{moment(date).fromNow()}</p>
+          <p>{title}</p>
+          <p>{excerpt}</p>
+          <Image src={image_url} alt={title} width={300} height={300} />
     </section>
   );
 }
