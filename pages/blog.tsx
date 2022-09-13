616c3b5cab2f9967f7f18d0631e754901bf7186d
Udoka <udochukwukaonyela@gmail.com>
2022-09-13 23:05:34 +0100

    fixes

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,9 +1,22 @@
 import type { NextPage } from 'next'
+import { useEffect } from "react";
+import { getPosts } from "../utils/services";
 
 export interface IblogProps {
 }
 
 const blog: NextPage<IblogProps> = () => {
+  useEffect(() => {
+    getPosts()
+    .then((res) => {
+      console.log(res);
+    }
+    )
+      .catch((err) => {
+        console.log(err);
+      }
+      );
+  }, [])
   return (
     <section>
       blog
