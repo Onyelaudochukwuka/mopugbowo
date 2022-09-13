92023819b305bb631f4338b58f8e7c1657484d3e
Udoka <udochukwukaonyela@gmail.com>
2022-09-13 16:11:41 +0100

    fixed up popUp

--- components/PopUp.tsx
+++ components/PopUp.tsx
@@ -2,13 +2,18 @@ import  React, { FC } from 'react';
 import { Success, Close } from "./icon";
 
 export interface IPopUpProps {
+  toggle: boolean;
+  close: () => void;
 }
 
-const PopUp: FC<IPopUpProps> = (props) => {
+const PopUp: FC<IPopUpProps> = ({ toggle, close }) => {
+  console.log(toggle);
   return (
-    <section className="flex bg-black/40 fixed inset-0 w-full h-full z-50 items-center justify-center">
+    <section className={`${toggle ? `scale-100`: `scale-0`} transition-transform duration-500 ease-in flex bg-black/40 fixed inset-0 w-full h-full z-50 items-center justify-center`}>
       <div className="bg-white w-3/4 h-3/4 relative flex items-center justify-center flex-col">
-        <Close className="w-8 h-8 absolute top-4 right-4 cursor-pointer" />
+        <Close
+          onClick={() => close()}
+          className="w-8 h-8 absolute top-4 right-4 cursor-pointer" />
         <p><Success /> </p>
         <p className="text-xl font-bold">Post Created</p>
       </div>
