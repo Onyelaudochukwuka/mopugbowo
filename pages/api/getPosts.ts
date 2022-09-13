e0bbd9b838f55e7f6058ecc08d5c1cd5ba4bd497
Udoka <udochukwukaonyela@gmail.com>
2022-09-13 22:21:41 +0100

    created getPosts endpoint

---
+++ pages/api/getPosts.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import dbConnect from '../../lib/dbConnect';
+const Blog = require('../../models/blog');
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { method } = req;
+    const { post, title, date, excerpt } = req.body;
+    await dbConnect()
+    switch (method) {
+        case 'GET':
+
+            try {
+                const blog = await Blog.find({});
+                res.status(201).json({ success: true, data: blog })
+            }
+            catch (error: any) {
+                console.log(error);
+                res.status(400).json({ success: false, message: error.message });
+            }
+            break;
+        default:
+            res.status(400).json({ success: false })
+            break
+    }
+}
