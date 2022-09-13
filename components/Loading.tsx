f56b1a8d059379f56ebea42cee30f4b8597c509c
Udoka <udochukwukaonyela@gmail.com>
2022-09-13 22:17:15 +0100

    styling

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,13 +1,22 @@
 import  React, { FC } from 'react';
 
 export interface ILoadingProps {
+  toggle: boolean;
 }
 
-const Loading: FC<ILoadingProps> = (props) => {
+const Loading: FC<ILoadingProps> = ({toggle}) => {
   return (
-    <section className={`${true ? `scale-100` : `scale-0`} transition-transform duration-500 ease-in flex bg-black/40 fixed inset-0 w-full h-full z-50 items-center justify-center`}>
-      
-    </section>
+  <section className={`${toggle ? `translate-x-0` : `-translate-x-full`} transition-transform duration-500 ease-in flex bg-black/40 fixed inset-0 w-full h-full z-50 items-center justify-center`}>
+    <div className="flex flex-col rounded-full w-36 h-36 justify-between animate-spin">
+        <div className="w-6 h-6 rounded-full bg-curves relative  self-center"></div>
+        
+        <div className="flex justify-between">
+      <div className="w-6 h-6 rounded-full bg-curves relative self-start"></div>
+      <div className="w-6 h-6 rounded-full bg-curves relative self-end"></div>
+        </div>
+      <div className="w-6 h-6 rounded-full bg-curves relative self-center"></div>
+    </div>
+  </section>
   );
 }
 export default Loading;
