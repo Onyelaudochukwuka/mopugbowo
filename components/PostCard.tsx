1f29cb8cd54529812988fbb193c82f60601eb11f
Udoka <udochukwukaonyela@gmail.com>
2022-09-13 23:48:19 +0100

    added postcards

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,5 +1,6 @@
 import moment from "moment";
 import Image from "next/image";
+import Link from "next/link";
 import  React, { FC } from 'react';
 import { BlogPost } from "../models/blog";
 
@@ -8,14 +9,43 @@ export interface IPostCardProps {
 
 const PostCard: FC<BlogPost> = ({ date, excerpt, image_url, post, title }) => {
   return (
-      <section className="flex flex-col">
-        <Image src={image_url} alt={title} width={300} height={300} />
-          <div className="flex flex-col text-white">
-          <p>{moment(date).fromNow()}</p>
-          <p>{title}</p>
-          <p>{excerpt}</p>
+      <div className="bg-curves text-white shadow-lg rounded-lg p-0 lg:pb-24 pb-8 mb-8 w-3/4 m-auto">
+          <div className="relative overflow-hidden shadow-md pb-80 mb-6">
+              <div
+                  className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg" >
+              <Image src={image_url} 
+                      layout="responsive"
+                      objectFit="cover"
+                      width={30}
+                      height={30}
+                    alt={title} />
+                  </div>
           </div>
-    </section>
+
+          <h1 className="transition duration-700 text-center mb-8 cursor-pointer
+      hover:text-blue-600 text-3xl font-semibold capitalize">
+              <Link href={`/post/${title}`}>{title.toLowerCase()}</Link>
+          </h1>
+          <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
+            
+              <div className="font-medium">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                  </svg>
+                  <span>
+                      {moment(date).format('MMM DD, YYYY')};
+                  </span>
+              </div>
+          </div>
+          <p className="text-center text-lg text-white font-normal px-4 lg:px-20 mb-8" >{excerpt}</p>
+          <div className="text-center">
+              <Link href={`/post/${title}`}>
+                  <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-primary hover:bg-dark text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
+                      Continue reading
+                  </span>
+              </Link>
+          </div>
+      </div>
   );
 }
 export default PostCard;
