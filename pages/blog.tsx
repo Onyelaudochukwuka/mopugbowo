59a6e4f6ca5d285be2c355fee4b47132363cd418
Udoka <udochukwukaonyela@gmail.com>
2022-09-13 22:18:33 +0100

    styling

---
+++ pages/blog.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+export interface IblogProps {
+}
+
+const blog: NextPage<IblogProps> = () => {
+  return (
+    <section>
+      blog
+    </section>
+  );
+}
+export default blog;
