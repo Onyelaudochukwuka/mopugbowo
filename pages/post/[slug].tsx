0f2b70bdd000bb850ed5a35d94b1a71bdc36269a
Udoka <udochukwukaonyela@gmail.com>
2022-09-14 21:25:53 +0100

    addd fetch logic to get initial props

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,168 +1,171 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 import React, { useEffect, useState } from "react";
-import { BlogPost } from "../../models/blog";
+import PostDetails from "../../components/PostDetails";
+import { BlogPost, ChildSchema } from "../../models/blog";
 import { fetchPost } from "../../utils/services";
 export interface IslugProps {
   slug: string;
+  data: BlogPost;
 }
 
-const slug: NextPage<IslugProps> = ({ slug }) => {
-  const [data, setData] = useState<BlogPost>(
-    {
-        "_id": "6321c1bc12a5be54300ef278",
-        "date": 1663156666654,
-        "image_url": "",
-        "title": "Testing",
-        "excerpt": "fbdbfhdffbfbhdf",
-        "slug": "testing",
-        "post": [
-          {
-            "type": "paragraph",
-            "children": [
-              {
-                "text": "This is editable ",
-                "type": "",
-                "bold": false,
-                "code": false,
-                "italic": false,
-                "underline": false,
-                "_id": "6321c1bc12a5be54300ef27a",
-                "children": []
-              },
-              {
-                "text": "rich",
-                "type": "",
-                "bold": true,
-                "code": false,
-                "italic": false,
-                "underline": false,
-                "_id": "6321c1bc12a5be54300ef27b",
-                "children": []
-              },
-              {
-                "text": " text, ",
-                "type": "",
-                "bold": false,
-                "code": false,
-                "italic": false,
-                "underline": false,
-                "_id": "6321c1bc12a5be54300ef27c",
-                "children": []
-              },
-              {
-                "text": "much",
-                "type": "",
-                "bold": false,
-                "code": false,
-                "italic": true,
-                "underline": false,
-                "_id": "6321c1bc12a5be54300ef27d",
-                "children": []
-              },
-              {
-                "text": " better than a ",
-                "type": "",
-                "bold": false,
-                "code": false,
-                "italic": false,
-                "underline": false,
-                "_id": "6321c1bc12a5be54300ef27e",
-                "children": []
-              },
-              {
-                "text": "<textarea>",
-                "type": "",
-                "bold": false,
-                "code": true,
-                "italic": false,
-                "underline": false,
-                "_id": "6321c1bc12a5be54300ef27f",
-                "children": []
-              },
-              {
-                "text": "!",
-                "type": "",
-                "bold": false,
-                "code": false,
-                "italic": false,
-                "underline": false,
-                "_id": "6321c1bc12a5be54300ef280",
-                "children": []
-              }
-            ],
-            "_id": "6321c1bc12a5be54300ef279"
-          },
-          {
-            "type": "paragraph",
-            "children": [
-              {
-                "text": "Since it's rich text, you can do things like turn a selection of text ",
-                "type": "",
-                "bold": false,
-                "code": false,
-                "italic": false,
-                "underline": false,
-                "_id": "6321c1bc12a5be54300ef282",
-                "children": []
-              },
-              {
-                "text": "bold",
-                "type": "",
-                "bold": true,
-                "code": false,
-                "italic": false,
-                "underline": false,
-                "_id": "6321c1bc12a5be54300ef283",
-                "children": []
-              },
-              {
-                "text": ", or add a semantically rendered block quote in the middle of the page, like this:",
-                "type": "",
-                "bold": false,
-                "code": false,
-                "italic": false,
-                "underline": false,
-                "_id": "6321c1bc12a5be54300ef284",
-                "children": []
-              }
-            ],
-            "_id": "6321c1bc12a5be54300ef281"
-          },
-          {
-            "type": "block-quote",
-            "children": [
-              {
-                "text": "A wise quote.",
-                "type": "",
-                "bold": false,
-                "code": false,
-                "italic": false,
-                "underline": false,
-                "_id": "6321c1bc12a5be54300ef286",
-                "children": []
-              }
-            ],
-            "_id": "6321c1bc12a5be54300ef285"
-          },
-          {
-            "type": "paragraph",
-            "children": [
-              {
-                "text": "Try it out for yourself!",
-                "type": "",
-                "bold": false,
-                "code": false,
-                "italic": false,
-                "underline": false,
-                "_id": "6321c1bc12a5be54300ef288",
-                "children": []
-              }
-            ],
-            "_id": "6321c1bc12a5be54300ef287"
-          }
-        ]
-    }
-  ); ;
+const slug: NextPage<IslugProps> = ({ slug, data }) => {
+  console.log(data, "dataStuff");
+  // const [data, setData] = useState<BlogPost>(
+  //   {
+  //       "_id": "6321c1bc12a5be54300ef278",
+  //       "date": 1663156666654,
+  //       "image_url": "",
+  //       "title": "Testing",
+  //       "excerpt": "fbdbfhdffbfbhdf",
+  //       "slug": "testing",
+  //       "post": [
+  //         {
+  //           "type": "paragraph",
+  //           "children": [
+  //             {
+  //               "text": "This is editable ",
+  //               "type": "",
+  //               "bold": false,
+  //               "code": false,
+  //               "italic": false,
+  //               "underline": false,
+  //               "_id": "6321c1bc12a5be54300ef27a",
+  //               "children": []
+  //             },
+  //             {
+  //               "text": "rich",
+  //               "type": "",
+  //               "bold": true,
+  //               "code": false,
+  //               "italic": false,
+  //               "underline": false,
+  //               "_id": "6321c1bc12a5be54300ef27b",
+  //               "children": []
+  //             },
+  //             {
+  //               "text": " text, ",
+  //               "type": "",
+  //               "bold": false,
+  //               "code": false,
+  //               "italic": false,
+  //               "underline": false,
+  //               "_id": "6321c1bc12a5be54300ef27c",
+  //               "children": []
+  //             },
+  //             {
+  //               "text": "much",
+  //               "type": "",
+  //               "bold": false,
+  //               "code": false,
+  //               "italic": true,
+  //               "underline": false,
+  //               "_id": "6321c1bc12a5be54300ef27d",
+  //               "children": []
+  //             },
+  //             {
+  //               "text": " better than a ",
+  //               "type": "",
+  //               "bold": false,
+  //               "code": false,
+  //               "italic": false,
+  //               "underline": false,
+  //               "_id": "6321c1bc12a5be54300ef27e",
+  //               "children": []
+  //             },
+  //             {
+  //               "text": "<textarea>",
+  //               "type": "",
+  //               "bold": false,
+  //               "code": true,
+  //               "italic": false,
+  //               "underline": false,
+  //               "_id": "6321c1bc12a5be54300ef27f",
+  //               "children": []
+  //             },
+  //             {
+  //               "text": "!",
+  //               "type": "",
+  //               "bold": false,
+  //               "code": false,
+  //               "italic": false,
+  //               "underline": false,
+  //               "_id": "6321c1bc12a5be54300ef280",
+  //               "children": []
+  //             }
+  //           ],
+  //           "_id": "6321c1bc12a5be54300ef279"
+  //         },
+  //         {
+  //           "type": "paragraph",
+  //           "children": [
+  //             {
+  //               "text": "Since it's rich text, you can do things like turn a selection of text ",
+  //               "type": "",
+  //               "bold": false,
+  //               "code": false,
+  //               "italic": false,
+  //               "underline": false,
+  //               "_id": "6321c1bc12a5be54300ef282",
+  //               "children": []
+  //             },
+  //             {
+  //               "text": "bold",
+  //               "type": "",
+  //               "bold": true,
+  //               "code": false,
+  //               "italic": false,
+  //               "underline": false,
+  //               "_id": "6321c1bc12a5be54300ef283",
+  //               "children": []
+  //             },
+  //             {
+  //               "text": ", or add a semantically rendered block quote in the middle of the page, like this:",
+  //               "type": "",
+  //               "bold": false,
+  //               "code": false,
+  //               "italic": false,
+  //               "underline": false,
+  //               "_id": "6321c1bc12a5be54300ef284",
+  //               "children": []
+  //             }
+  //           ],
+  //           "_id": "6321c1bc12a5be54300ef281"
+  //         },
+  //         {
+  //           "type": "block-quote",
+  //           "children": [
+  //             {
+  //               "text": "A wise quote.",
+  //               "type": "",
+  //               "bold": false,
+  //               "code": false,
+  //               "italic": false,
+  //               "underline": false,
+  //               "_id": "6321c1bc12a5be54300ef286",
+  //               "children": []
+  //             }
+  //           ],
+  //           "_id": "6321c1bc12a5be54300ef285"
+  //         },
+  //         {
+  //           "type": "paragraph",
+  //           "children": [
+  //             {
+  //               "text": "Try it out for yourself!",
+  //               "type": "",
+  //               "bold": false,
+  //               "code": false,
+  //               "italic": false,
+  //               "underline": false,
+  //               "_id": "6321c1bc12a5be54300ef288",
+  //               "children": []
+  //             }
+  //           ],
+  //           "_id": "6321c1bc12a5be54300ef287"
+  //         }
+  //       ]
+  //   }
+  // ); ;
   const getContentFragment = (index: number, text: any, obj: any, type?:string) => {
     let modifiedText: any = text;
 
@@ -231,11 +234,11 @@ const slug: NextPage<IslugProps> = ({ slug }) => {
         return modifiedText
     }
   };
-  useEffect(() => {
-    fetchPost(slug)
-      .then((res) => { console.log(res.data); setData(res.data); })
-      .catch((err) => console.log(err));
-  }, [slug]);
+  // useEffect(() => {
+  //   fetchPost(slug)
+  //     .then((res) => { console.log(res.data); setData(res.data); })
+  //     .catch((err) => console.log(err));
+  // }, [slug]);
   useEffect(() => {
     console.log(data, "data");
   }, [data]);
@@ -245,14 +248,8 @@ const slug: NextPage<IslugProps> = ({ slug }) => {
       {
         !!data
       
-      ?
-        data.post.map((typeObj, index) => {
-        const { type, children } = typeObj;
-
-        const child = children.map((item, itemindex) => !!item.type ? getContentFragment(index, item.children.map((item, itemIndex) => getContentFragment(itemindex, item?.text, item)), typeObj, item.type) : getContentFragment(itemindex, !!item.text ? item.text : "", item));
-
-          return getContentFragment(index, child, typeObj, type);
-        })
+          ?
+          <PostDetails {...data} />
         :
         <p>No Post in this category</p>
       }
@@ -265,6 +262,7 @@ const slug: NextPage<IslugProps> = ({ slug }) => {
 }
 slug.getInitialProps = async ({ query }: any) => {
   const { slug } = query;
-  return { slug };
+  const data = await fetchPost(slug);
+  return { slug, data: data.data };
 }
 export default slug;
