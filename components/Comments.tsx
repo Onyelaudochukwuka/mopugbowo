21f3d18ff4444b9c366258ac38fd554f9976805e
Udoka <udochukwukaonyela@gmail.com>
2022-09-14 21:25:01 +0100

    created component for getting comments

---
+++ components/Comments.tsx
@@ -0,0 +1,13 @@
+import  React, { FC } from 'react';
+
+export interface ICommentsProps {
+}
+
+const Comments: FC<ICommentsProps> = (props) => {
+  return (
+    <section>
+      Comments
+    </section>
+  );
+}
+export default Comments;
