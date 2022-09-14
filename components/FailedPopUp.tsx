16d96ea7663eb497a2dc6380a5bf307792ee79d9
Udoka <udochukwukaonyela@gmail.com>
2022-09-14 13:45:56 +0100

    created failed pop up

--- components/FailedPopUp.tsx
+++ components/FailedPopUp.tsx
@@ -1,20 +1,21 @@
 import  React, { FC } from 'react';
-import { Success, Close } from "./icon";
+import { Success, Close, Failed } from "./icon";
 
 export interface IPopUpProps {
   toggle: boolean;
   close: () => void;
+  message?: string;
 }
 
-const FailedPopUp: FC<IPopUpProps> = ({ toggle, close }) => {
+const FailedPopUp: FC<IPopUpProps> = ({ toggle, close, message }) => {
   return (
     <section className={`${toggle ? `scale-100`: `scale-0`} transition-transform duration-500 ease-in flex bg-black/40 fixed inset-0 w-full h-full z-50 items-center justify-center`}>
       <div className="bg-white w-3/4 h-3/4 relative flex items-center justify-center flex-col">
         <Close
           onClick={() => close()}
           className="w-8 h-8 absolute top-4 right-4 cursor-pointer" />
-        <p><Success /> </p>
-        <p className="text-xl font-bold">Post Created</p>
+        <p><Failed /> </p>
+        <p className="text-xl font-bold">{ message }</p>
       </div>
     </section>
   );
