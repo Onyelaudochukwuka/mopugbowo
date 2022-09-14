0ade7a0794cdcb0455266fe65b49eaeb09eb1a9b
Udoka <udochukwukaonyela@gmail.com>
2022-09-14 22:30:32 +0100

    styling

--- components/PostDetails.tsx
+++ components/PostDetails.tsx
@@ -77,7 +77,7 @@ const PostDetails: FC<PropTypes<IPostDetailsProps, BlogPost>> = ({ post, image_u
     }
   };
   return (
-      <div className="bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
+    <div className="bg-[#711C91] text-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8 w-3/4 mx-auto">
         <div className="relative overflow-hidden shadow-md mb-6">
         <div
           className="object-top relative h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg" >
@@ -91,14 +91,14 @@ const PostDetails: FC<PropTypes<IPostDetailsProps, BlogPost>> = ({ post, image_u
         </div>
         <div className="px-4 lg:px-0">
           <div className="flex items-center mb-8 w-full">
-            <div className="font-medium text-gray-700">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <div className="font-medium text-white">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 fill-curves" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
               </svg>
             <span className="align-middle">{moment(date).format('MMM DD, YYYY')}</span>
             </div>
           </div>
-          <h1 className="mb-8 text-3xl font-semibold">{title}</h1>
+          <h1 className="mb-8 lg:text-5xl text-3xl font-bold text-center">{title}</h1>
         {post.map((typeObj, index) => {
           const { type, children } = typeObj;
 
