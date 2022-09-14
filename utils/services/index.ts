08065b594dc8973c3da52a69faed2dd34fa9180c
Udoka <udochukwukaonyela@gmail.com>
2022-09-14 21:26:00 +0100

    fixes

--- utils/services/index.ts
+++ utils/services/index.ts
@@ -20,7 +20,7 @@ export const getPosts = async () => {
     return res.json()
 }
 export const fetchPost = async (slug:string) => { 
-    const res = await fetch(`/api/fetchPost?slug=${slug}`, {
+    const res = await fetch(`http://localhost:3000/api/fetchPost?slug=${slug}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
