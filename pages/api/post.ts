9e1faeb3579e0e6c276bd42ec160ac7a008f924c
Udoka <udochukwukaonyela@gmail.com>
2022-09-14 13:13:21 +0100

    fixed api logic

--- pages/api/post.ts
+++ pages/api/post.ts
@@ -3,14 +3,19 @@ import dbConnect from '../../lib/dbConnect';
 const Blog = require('../../models/blog');
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { method } = req;
-    const { post, title, date, excerpt } = req.body;
+    const { post, title, date, excerpt, slug, image_url } = req.body;
     await dbConnect()
     switch (method) {
         case 'POST':
 
-                try {
-                    const blog = await Blog.create({ post, title, date, excerpt });
+            try {
+                const exists = await Blog.find({ slug: { $eq: req.body.slug } });
+                if (exists.length === 0) {
+                    const blog = await Blog.create(req.body);
                     res.status(201).json({ success: true, data: blog })
+                } else {
+                    res.status(400).json({ success: false, message: "Post already exists" })
+                }
                 }
                 catch (error: any) {
                     console.log(error);
