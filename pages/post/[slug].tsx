fd05058444934f030d11e18eb12f6ece3af43c02
Udoka <udochukwukaonyela@gmail.com>
2022-09-14 13:13:12 +0100

    added logic to convert rich text

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,13 +1,95 @@
 import type { NextPage } from 'next'
-
+import React from "react";
+import data from "../../index"
 export interface IslugProps {
   slug: string;
 }
 
-const slug: NextPage<IslugProps> = ({ slug }) =>{
+const slug: NextPage<IslugProps> = ({ slug }) => {
+  const getContentFragment = (index: number, text: any, obj: any, type?:string) => {
+    let modifiedText: any = text;
+
+    if (obj.bold) {
+      modifiedText = <strong>{text}</strong>
+    }
+
+    if (obj.code) {
+      modifiedText = <code className="font-mono p-[3px] bg-[rgb(238,238,238)]">{text}</code>
+    }
+
+    if (obj.italic) {
+      modifiedText = <em>{text}</em>
+    }
+
+    if (obj.underline) {
+      modifiedText = <u>{text}</u>
+    }
+
+
+
+    switch (type) {
+      case 'block-quote':
+        return (
+          <blockquote className="italic text-[rgb(170,170,170)] pl-[10px] border-l-2 border-solid border-[rgb(221,221,221)]">
+            {modifiedText.map((item: JSX.Element, i: number) => <React.Fragment key={i}>{item}</React.Fragment>)}
+          </blockquote>
+        )
+      case 'bulleted-list':
+        return (
+          <ul className="list-disc p-6">
+            {modifiedText.map((item: JSX.Element, i: number) => <React.Fragment key={i}>{item}</React.Fragment>)}
+          </ul>
+        )
+      case 'heading-one':
+        return (
+          <h1 className="text-xl font-bold">
+            {modifiedText.map((item: JSX.Element, i: number) => <React.Fragment key={i}>{item}</React.Fragment>)}
+          </h1>
+        )
+      case 'heading-two':
+        return (
+          <h2 className="text-lg font-bold">
+            {modifiedText.map((item: JSX.Element, i: number) => <React.Fragment key={i}>{item}</React.Fragment>)}
+          </h2>
+        )
+      case 'list-item':
+        return (
+          <li>
+            {modifiedText.map((item: JSX.Element, i: number) => <React.Fragment key={i}>{item}</React.Fragment>)}
+          </li>
+        )
+      case 'numbered-list':
+        return (
+          <ol className="list-decimal p-6">
+            {modifiedText.map((item: JSX.Element, i: number) => <React.Fragment key={i}>{item}</React.Fragment>)}
+          </ol>
+        )
+      case 'paragraph':
+        return (
+          <p>
+            {modifiedText.map((item: JSX.Element, i: number) => <React.Fragment key={i}>{item}</React.Fragment>)}
+          </p>
+        )
+      default:
+        return modifiedText
+    }
+  };
+  console.log(data.data[data.data.length - 1]);
   return (
     <section>
-      slug
+      
+      {data.data[data.data.length - 1].post.map((typeObj, index) => {
+        const { type, children } = typeObj;
+
+        const child = children.map((item, itemindex) => !!item.type ? getContentFragment(index, item.children.map((item, itemIndex) => getContentFragment(itemindex, item?.text, item)), typeObj, item.type) : getContentFragment(itemindex, !!item.text ? item.text : "", item));
+
+          return getContentFragment(index, child, typeObj, type);
+        })
+      }
+  
+     
+      
+        
     </section>
   );
 }
