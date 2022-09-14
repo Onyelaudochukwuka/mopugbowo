41127484309acb91ae4536d5324abe588e07dcff
Udoka <udochukwukaonyela@gmail.com>
2022-09-14 21:25:09 +0100

    created comments form

---
+++ components/CommentsForm.tsx
@@ -0,0 +1,13 @@
+import  React, { FC } from 'react';
+
+export interface ICommentsFormProps {
+}
+
+const CommentsForm: FC<ICommentsFormProps> = (props) => {
+  return (
+    <section>
+      CommentsForm
+    </section>
+  );
+}
+export default CommentsForm;
