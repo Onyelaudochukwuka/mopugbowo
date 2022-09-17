77eaf64f434ff3e24acd15999d364485089af57a
Udoka <udochukwukaonyela@gmail.com>
2022-09-17 21:24:57 +0100

    styling

--- components/PostDetails.tsx
+++ components/PostDetails.tsx
@@ -77,22 +77,22 @@ const PostDetails: FC<PropTypes<IPostDetailsProps, BlogPost>> = ({ post, image_u
     }
   };
   return (
-    <div className="bg-[#711C91] text-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8 w-3/4 mx-auto">
-        <div className="relative overflow-hidden shadow-md mb-6">
+    <div className="bg-light text-primaryLight shadow-lg rounded-lg lg:p-8 pb-12 mb-8 w-3/4 mx-auto">
+      <div className="relative overflow-hidden shadow-md pb-80 mb-6">
         <div
-          className="object-top relative h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg" >
+          className="object-top relative h-80 w-full object-cover rounded-t-lg lg:rounded-lg" >
           <Image src={image_url}
             layout="responsive"
-            objectFit="cover"
-            width={300}
-            height={300}
+            objectFit="scale-down"
+            width={30}
+            height={30}
             alt={title} />
         </div>
-        </div>
+      </div>
         <div className="px-4 lg:px-0">
           <div className="flex items-center mb-8 w-full">
-            <div className="font-medium text-white">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 fill-curves" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <div className="font-medium text-primaryLight">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 text-light fill-light w-6 inline mr-2 fill-curves" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
               </svg>
             <span className="align-middle">{moment(date).format('MMM DD, YYYY')}</span>
