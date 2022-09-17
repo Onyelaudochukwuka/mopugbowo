6c6684742930cc492ba46a11d28e912a51a6cea6
Udoka <udochukwukaonyela@gmail.com>
2022-09-17 21:23:20 +0100

    fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,11 @@ module.exports = {
     colors: {
         shadow: "#D9D9D9",
         primary: "#550984",
-        curves: "#B641FF",
+        curves: "#A287AF",
         dark: "#4E0B77",
-        primaryLight: "#C363FF",
-        light: "#F2B80E",
+        primaryLight: "#A287AF",
+        light: "#FFF6FF",
+        text: "#fff"
       },
       backgroundImage: {background: "url(../public/background.jpeg)"},
     },
