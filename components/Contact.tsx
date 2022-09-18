866502357208a2ea676b5e101b10af8b4e373528
Udoka <udochukwukaonyela@gmail.com>
2022-09-18 19:54:50 +0100

    added id

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -28,7 +28,7 @@ const contactDetails:ContactDetails = [
 const Contact: FC = () => {
   const Id = useId();
   return (
-    <section className="p-3 flex flex-col gap-8">
+    <section className="p-3 flex flex-col gap-8" id="contact-us">
       <h1 className="text-center relative text-bold text-text font-bold tracking-widest text-2xl before:absolute before:w-24 before:h-1 before:rounded-full  before:bg-text before:mt-8 before:origin-left before:transition-[transform] before:duration-300 hover:before:scale-x-125  cursor-pointer">Contact Us</h1>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.107868914145!2d5.604486662757621!3d6.380075390984419!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcb4f67723b60b2ea!2sMinistry%20Of%20Perfection%20-%20Ugbowo!5e0!3m2!1sen!2sng!4v1653662210346!5m2!1sen!2sng"
