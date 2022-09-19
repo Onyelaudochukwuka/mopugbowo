8cfb8e15e588205247ff9091d5f9c1d4c48ce116
Udoka <udochukwukaonyela@gmail.com>
2022-09-19 12:29:09 +0100

    added on animations

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -6,9 +6,9 @@ export interface ILoaderProps {
 const Loader: FC<ILoaderProps> = (props) => {
   return (
     <section>
-      <div className="w-8 h-8 bg-light "></div>
-      <div className="w-8 h-8 bg-light "></div>
-      <div className="w-8 h-8 bg-light "></div>
+      <div className="w-8 h-8 bg-light animate1"></div>
+      <div className="w-8 h-8 bg-light animate2"></div>
+      <div className="w-8 h-8 bg-light animate3"></div>
     </section>
   );
 }
