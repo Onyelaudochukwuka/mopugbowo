acc0c0ce5e2e467fd6fcde902ac6394522194d6d
Udoka <udochukwukaonyela@gmail.com>
2022-09-19 07:38:53 +0100

    stytling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
 import { About, Contact, Cta, Hero, Sermons, Card } from "../components"
+import Gallery from "../components/Gallery"
 const Home: NextPage = () => {
   return (
     <div>
       <Hero />
       <Sermons />
+      <Gallery />
       <About />
       <Contact />
       <Cta/>
