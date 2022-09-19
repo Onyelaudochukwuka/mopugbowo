2531fa4aca3f8d4ef84083faf8f2174a58310672
Udoka <udochukwukaonyela@gmail.com>
2022-09-19 12:51:52 +0100

    fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app'
 import { Layout } from "../components";
+import { Provider } from "react-redux";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
   )
 }
 
