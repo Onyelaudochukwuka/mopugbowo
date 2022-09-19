5b5eab616dd25292a5eb0453547cdacb0c921468
Udoka <udochukwukaonyela@gmail.com>
2022-09-19 07:38:47 +0100

    imported styleshees

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import "../components/styles.css"
 import type { AppProps } from 'next/app'
 import { Layout } from "../components";
 function MyApp({ Component, pageProps }: AppProps) {
