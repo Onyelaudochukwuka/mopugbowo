fdebb0c96a1feaf29204f9cacb980b5e22e36592
Udoka <udochukwukaonyela@gmail.com>
2022-09-19 12:33:11 +0100

    formatted document

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,22 +1,26 @@
-import  React, { FC } from 'react';
+import React, { FC } from "react";
 
 export interface ILoadingProps {
   toggle: boolean;
 }
 
-const Loading: FC<ILoadingProps> = ({toggle}) => {
+const Loading: FC<ILoadingProps> = ({ toggle }) => {
   return (
-  <section className={`${toggle ? `translate-x-0` : `-translate-x-full`} transition-transform duration-500 ease-in flex bg-black/40 fixed inset-0 w-full h-full z-50 items-center justify-center`}>
-    <div className="flex flex-col rounded-full w-36 h-36 justify-between animate-spin">
+    <section
+      className={`${
+        toggle ? `translate-x-0` : `-translate-x-full`
+      } transition-transform duration-500 ease-in flex bg-black/40 fixed inset-0 w-full h-full z-50 items-center justify-center`}
+    >
+      <div className="flex flex-col rounded-full w-36 h-36 justify-between animate-spin">
         <div className="w-6 h-6 rounded-full bg-curves relative  self-center"></div>
-        
+
         <div className="flex justify-between">
-      <div className="w-6 h-6 rounded-full bg-curves relative self-start"></div>
-      <div className="w-6 h-6 rounded-full bg-curves relative self-end"></div>
+          <div className="w-6 h-6 rounded-full bg-curves relative self-start"></div>
+          <div className="w-6 h-6 rounded-full bg-curves relative self-end"></div>
         </div>
-      <div className="w-6 h-6 rounded-full bg-curves relative self-center"></div>
-    </div>
-  </section>
+        <div className="w-6 h-6 rounded-full bg-curves relative self-center"></div>
+      </div>
+    </section>
   );
-}
+};
 export default Loading;
