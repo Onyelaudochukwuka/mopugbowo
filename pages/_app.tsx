5d5552f365fcb1e16f48265ae9287dd85f9b7c4b
Udoka <udochukwukaonyela@gmail.com>
2022-09-20 09:00:15 +0100

    added providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app'
 import { Layout } from "../components";
 import { Provider } from "react-redux";
+import store from "../utils/services/store";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <Provider store={store}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
+    </Provider>
   )
 }
 
