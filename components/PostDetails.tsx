e0ea7e9590d8c8fba8a140bfe4fd5d80ca2ce55e
Udoka <udochukwukaonyela@gmail.com>
2022-09-21 21:10:13 +0100

    fixes

--- components/PostDetails.tsx
+++ components/PostDetails.tsx
@@ -91,8 +91,8 @@ const PostDetails: FC<PropTypes<IPostDetailsProps, BlogPost>> = ({ post, image_u
       </div>
         <div className="px-4 lg:px-0">
           <div className="flex items-center mb-8 w-full">
-            <div className="font-medium text-primaryLight">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 text-light fill-light w-6 inline mr-2 fill-curves" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <div className="font-medium text-white">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 text-white w-6 inline mr-2 fill-curves" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
               </svg>
             <span className="align-middle">{moment(date).format('MMM DD, YYYY')}</span>
