1ec670f701c8acd85767aceb946ed2f354910234
Udoka <udochukwukaonyela@gmail.com>
2022-09-21 21:07:30 +0100

    responsiveness fixes

--- components/Slides.tsx
+++ components/Slides.tsx
@@ -8,7 +8,7 @@ const Slides: FC = () => {
 
   return (
     <div className="flex w-full items-center justify-center mt-24 z-10 relative">
-      <span className="w-16 h-16 block lg:translate-x-56 cursor-pointer lg:relative absolute -left-6 z-50"
+      <span className="w-10 h-10 lg:w-16 lg:h-16 block lg:translate-x-56 cursor-pointer lg:relative absolute -left-6 z-50"
         onClick={()=>{
           setImages((prev)=>{
             const newArr = [...prev];
@@ -23,7 +23,7 @@ const Slides: FC = () => {
           <span key={Id + i} className={`${i % 2 !== 1 ? `shadow-[0px_0px_15px_7.5px_rgba(0,0,0,0.25)] hover:shadow-[0px_0px_20px_10px_rgba(0,0,0,0.25)] ${(i + 1) % 3 !== 0 ? `lg:translate-x-36` : `lg:-translate-x-36`}` : `z-20 scale-150 shadow-[0px_0px_25px_12.5px_rgba(0,0,0,0.25)] hover:shadow-[0px_0px_40px_20px_rgba(0,0,0,0.25)]`} lg:w-1/2 w-[100vw] h-auto block transition-all duration-500 ease-in basis-full`}><Image src={img} layout={"responsive"} /></span>
           )}
       </div>
-      <span className="w-16 h-16 lg:-translate-x-56 lg: block cursor-pointer absolute lg:relative -right-6  z-50"
+      <span className="w-10 h-10 lg:-translate-x-56 lg:w-16 lg:h-16 block cursor-pointer absolute lg:relative -right-6  z-50"
         onClick={() => {
           setImages((prev) => {
             const newArr = [...prev];
