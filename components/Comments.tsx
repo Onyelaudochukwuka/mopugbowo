567d33b23884a5cf92fe52173e9756c1958993e6
Udoka <udochukwukaonyela@gmail.com>
2022-09-21 10:35:16 +0100

    fixes

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,9 +1,15 @@
 import  React, { FC } from 'react';
+import { useGetCommentsQuery } from "../utils/services/ApiConnection";
 
 export interface ICommentsProps {
+  slug: string;
 }
 
-const Comments: FC<ICommentsProps> = (props) => {
+const Comments: FC<ICommentsProps> = ({ slug }) => {
+  const { data, isLoading, refetch } = useGetCommentsQuery(slug, {
+    pollingInterval: 3000
+  });
+  console.log(data, isLoading);
   return (
     <section>
       Comments
