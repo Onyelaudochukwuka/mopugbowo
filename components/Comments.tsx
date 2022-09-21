24e02a159163a20332696c79f0a0a9711f146640
Udoka <udochukwukaonyela@gmail.com>
2022-09-21 19:27:40 +0100

    added comments

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -11,11 +11,13 @@ const Comments: FC<ICommentsProps> = ({ slug }) => {
     pollingInterval: 3000
   });
   return (
-      <div className="bg-primaryLight text-white shadow-lg lg:w-3/4 w-10/12 mx-auto  rounded-lg p-8 pb-12 mb-8">
+  <>
+    {  (comments?.length ?? 0) > 0 && 
+    (<div className="bg-primaryLight text-white shadow-lg lg:w-3/4 w-10/12 mx-auto  rounded-lg p-8 pb-12 mb-8">
         <h3 className="text-xl mb-8 font-semibold border-b pb-4">
           {comments?.length}
           {' '}
-          Comments
+          Comment{((comments?.length ?? 0) > 1) && "s"}
         </h3>
         {comments?.map(({date, email, name, comment}) => (
           <div className="border-b border-gray-100 mb-4 pb-4">
@@ -29,7 +31,9 @@ const Comments: FC<ICommentsProps> = ({ slug }) => {
             <p className="whitespace-pre-line text-white w-full">{comment}</p>
           </div>
          ))}
-      </div>
+    </div>)
+      }
+    </>
   );
 }
 export default Comments;
