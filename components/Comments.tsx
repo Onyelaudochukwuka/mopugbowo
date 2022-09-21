c05a8ae1e6ff98f7e8b974f6ebd38cf609b79ddf
Udoka <udochukwukaonyela@gmail.com>
2022-09-21 10:53:20 +0100

    added comments

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,3 +1,4 @@
+import moment from "moment";
 import  React, { FC } from 'react';
 import { useGetCommentsQuery } from "../utils/services/ApiConnection";
 
@@ -6,14 +7,29 @@ export interface ICommentsProps {
 }
 
 const Comments: FC<ICommentsProps> = ({ slug }) => {
-  const { data, isLoading, refetch } = useGetCommentsQuery(slug, {
+  const { data: comments, isLoading, refetch } = useGetCommentsQuery(slug, {
     pollingInterval: 3000
   });
-  console.log(data, isLoading);
   return (
-    <section>
-      Comments
-    </section>
+      <div className="bg-primaryLight text-white shadow-lg lg:w-3/4 w-10/12 mx-auto  rounded-lg p-8 pb-12 mb-8">
+        <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+          {comments?.length}
+          {' '}
+          Comments
+        </h3>
+        {comments?.map(({date, email, name, comment}) => (
+          <div className="border-b border-gray-100 mb-4 pb-4">
+            <p className="mb-4">
+              <span className="font-semibold">{name}</span>
+              {' '}
+              on
+              {' '}
+              {moment(date).format('MMM DD, YYYY')}
+            </p>
+            <p className="whitespace-pre-line text-white w-full">{comment}</p>
+          </div>
+         ))}
+      </div>
   );
 }
 export default Comments;
