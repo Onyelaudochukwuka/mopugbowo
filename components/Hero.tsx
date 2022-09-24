6762fbeb72aaa80618649cbcb94a03f27b19f26c
Udoka <udochukwukaonyela@gmail.com>
2022-09-24 15:02:29 +0100

    fixes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero: FC = () => {
   return (
     <div
       className="h-screen bg-background bg-center bg-cover bg-no-repeat flex justify-center items-center flex-col text-text font-bold gap-4 relative">
-      <p className="lg:text-3xl font-extrabold text-center text-lg">Church Is Not Just The Physical Building<br /> But It Is The Ground And Pillar Of Truth. <br /> 1 Timothy 3:15</p>
+      <p className="lg:text-3xl font-extrabold text-center text-lg">Church Is Not Just The Physical Building<br /> But It Is The Ground And Pillar Of Truth. <br /><span className="text-base"> 1 Timothy 3:15</span></p>
       <a href="" className="bg-light text-primaryLight p-3 px-5 text-sm lg:text-base rounded-full  hover:bg-light/75 transition-[background] duration-500 ease-in relative cursor-pointer"> Watch Sermon </a>
     </div>
   )
