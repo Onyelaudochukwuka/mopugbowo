f6bff008a738c60494d444d33843841110d4f750
Udoka <udochukwukaonyela@gmail.com>
2022-09-25 20:28:18 +0100

    created events page

---
+++ pages/events.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+export interface IEventsProps {
+}
+
+const events: NextPage<IEventsProps> = () => {
+  return (
+    <section>
+      events
+    </section>
+  );
+}
+export default events;
