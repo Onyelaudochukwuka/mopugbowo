f8795b869d8d68b05a8658966bb172d03cc8b012
Udoka <udochukwukaonyela@gmail.com>
2022-12-27 21:33:51 +0100

    feat: added new image

--- public/index.js
+++ public/index.js
@@ -1,28 +1,59 @@
-import logo from "./logo.png";
-import wave1 from "./wave1.svg";
-import slide1 from './slide1.jpeg';
-import slide2 from './slide2.jpeg';
-import slide3 from './slide3.jpeg';
-import leftArrow from './leftarrow.svg';
-import rightArrow from './rightarrow.svg';
 import about1 from './about1.png';
 import about2 from './about2.png';
 import about3 from './about3.png';
-import pastor from './pastor.png';
-import location from './location.svg';
-import mail from './mail.svg';
-import phone from './phone.svg';
-import footerimg from './footerimg.svg';
+import Arrow from './Arrow';
+import Cancel from './Cancel';
+import crossOverInvitation from './CrossOverinvitation.png';
 import email from './email.svg';
 import facebook from './facebook.svg';
-import twitter from './twitter.svg';
+import footerimg from './footerimg.svg';
+import Gallery1 from './GallerySlide1.jpg';
+import Gallery2 from './GallerySlide2.jpg';
+import Gallery3 from './GallerySlide3.jpg';
+import Gallery4 from './GallerySlide4.jpeg';
+import Gallery5 from './GallerySlide5.jpg';
 import instagram from './instagram.svg';
-import Cancel from './Cancel';
-import Arrow from './Arrow';
-import Gallery1 from "./GallerySlide1.jpg";
-import Gallery2 from "./GallerySlide2.jpg";
-import Gallery3 from "./GallerySlide3.jpg";
-import Gallery4 from "./GallerySlide4.jpeg";
-import Gallery5 from "./GallerySlide5.jpg";
+import leftArrow from './leftarrow.svg';
+import location from './location.svg';
+import logo from './logo.png';
+import mail from './mail.svg';
+import christmas from './MinistryOfPerfectionChrismaswishes.png';
+import pastor from './pastor.png';
+import phone from './phone.svg';
+import rightArrow from './rightarrow.svg';
+import slide1 from './slide1.jpeg';
+import slide2 from './slide2.jpeg';
+import slide3 from './slide3.jpeg';
+import twitter from './twitter.svg';
+import wave1 from './wave1.svg';
 
-export { logo, wave1, slide1, slide2, slide3, rightArrow, leftArrow, about1, about2, about3, pastor, location, phone, mail, footerimg, email, instagram, twitter, facebook, Cancel, Arrow, Gallery1, Gallery2, Gallery3, Gallery4, Gallery5 };
+export {
+  logo,
+  wave1,
+  slide1,
+  slide2,
+  slide3,
+  rightArrow,
+  leftArrow,
+  about1,
+  about2,
+  about3,
+  christmas,
+  crossOverInvitation,
+  pastor,
+  location,
+  phone,
+  mail,
+  footerimg,
+  email,
+  instagram,
+  twitter,
+  facebook,
+  Cancel,
+  Arrow,
+  Gallery1,
+  Gallery2,
+  Gallery3,
+  Gallery4,
+  Gallery5,
+};
