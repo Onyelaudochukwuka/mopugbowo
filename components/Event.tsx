9b6339603614e08bc7b087b9be752050f6da4d60
Udoka <udochukwukaonyela@gmail.com>
2022-12-27 21:34:37 +0100

    feat: fixed imports

--- components/Event.tsx
+++ components/Event.tsx
@@ -1,11 +1,21 @@
 import React, { FC } from 'react';
 
+import moment from 'moment';
+import Image, { StaticImageData } from 'next/image';
 import Countdown, { CountdownRendererFn } from 'react-countdown';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface IEventProps {}
+export interface IEventProps {
+  title: string
+  date: string
+  time: string
+  description: string
+  image: StaticImageData
+  timeLeft: number
+}
 
 const Renderer: CountdownRendererFn = ({
+  days,
   hours,
   minutes,
   seconds,
@@ -18,7 +28,9 @@ const Renderer: CountdownRendererFn = ({
   }
   // Render a countdown
   return (
-    <span>
+    <span className="text-3xl font-bold">
+      {days}
+      :
       {hours}
       :
       {minutes}
@@ -27,7 +39,29 @@ const Renderer: CountdownRendererFn = ({
     </span>
   );
 };
-const Event: FC<IEventProps> = () => {
-  return <Countdown date={Date.now() + 5000} renderer={Renderer} />;
+const Event: FC<IEventProps> = ({
+  timeLeft,
+  title,
+  date,
+  time,
+  description,
+  image,
+}) => {
+  return (
+    <div className="flex items-center justify-center lg:w-10/12 w-11/12 bg-white p-3 rounded-md shadow-xl gap-4">
+      <div className="basis-1/2">
+        <Image src={image} layout="responsive" />
+      </div>
+      <div className="basis-1/2 flex flex-col justify-between h-full lg:text-center">
+        <Countdown date={timeLeft} renderer={Renderer} />
+        <div>
+          <h2 className="lg:text-xl text-lg font-bold">{title}</h2>
+          <p className="text-base font-bold">{moment(date).format('Do MMMM, YYYY')}</p>
+          <p className="font-bold">{time}</p>
+          <p className="hidden">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Event;
